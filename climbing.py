a1a2459dd2f761e43e89c8fb27a2e875f3e03ad2
andreuntem <andre.untem@gmail.com>
2020-02-03 02:15:33 +0000

    with binary fail timeout

--- climbing.py
+++ climbing.py
@@ -1,40 +1,56 @@
+def binary_search(board_score, alice_score, i_min):
+
+    # start algorithm
+    i_max = len(board_score)-1
+    i_mid = (i_min+i_max)//2
+    d = i_max - i_min
+
+    while d>2:
+        score_mid = board_score[i_mid]
+        if alice_score<=score_mid:
+            i_max = i_mid
+        else:
+            i_min = i_mid
+        i_mid = (i_min+i_max)//2
+        d = i_max - i_min
+ 
+    score_min, score_mid, score_max = board_score[i_min], board_score[i_mid], board_score[i_max]
+    if score_min <= alice_score < score_mid:
+        ind = i_min
+    elif score_mid <= alice_score < score_max:
+        ind = i_mid
+    elif alice_score == score_max:
+        ind = i_max
+    pos = len(board_score) - ind
+
+    return ind, pos
+
+
 def climbingLeaderboard(scores, alice):
     
     # create unique sore in ascending order:
-    board_score = sorted(list(set(scores)))
-    n_board = len(board_score)
+    board_score = sorted(set(scores))
     alice_position = []
 
-    # Look for minimum value
-    flag_minimum = [i if alice[0]>board_score[i] else -999 for i in range(n_board)]
-
-    pos_inv = max(max(flag_minimum),0)
-    for alice_score in alice:
-        if alice_score >= max(board_score):
-            position = 1
+    ind = 0
+    for alice_score in alice:    
+        if alice_score < min(board_score):
+            pos = len(board_score)+1
+        elif alice_score > max(board_score):
+            pos = 1
         else:
-            while pos_inv < n_board:
-                score = board_score[pos_inv]
-                if alice_score < score:
-                    position = n_board - pos_inv + 1
-                    break
-                elif alice_score == score:
-                    position = n_board - pos_inv
-                    break
-                pos_inv += 1
-        
-        alice_position.append(position)
-    
+            ind, pos = binary_search(board_score, alice_score, ind)
+        alice_position.append(pos)
+
     return alice_position
-            
 
 
 if __name__ == '__main__':
     str_inp = '''
-    7
-    100 100 50 40 40 20 10
-    4
-    15 25 50 120
+    6
+    100 90 90 80 75 60
+    5
+    50 65 77 90 102
     '''
     inp = list(map(int,str_inp.split()))
     n_scores = inp[0]
